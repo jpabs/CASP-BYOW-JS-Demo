203d49dd3778cc56d8f626375c8685b9aa016b56
adamunbound <adam.ilan@unbound-tech.com>
2018-12-27 10:39:16 +0200

    check node version on startup

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var test = require('./src/test');
+var checkEngine = require('check-engine');
 
-test();
+checkEngine().then((result) => {
+  if (result.status !== 0) {
+      console.log('Failed validating node version');
+  } else {
+    if((result.message || {}).type !== 'success') {
+      console.log(result.message.text);
+      result.packages.forEach(p => {
+        if(p.type === 'error') {
+          console.log(`'${p.name}' has invalid version; expected version is: ${p.expectedVersion}, existing version is: ${p.foundVersion}`);
+        }
+      })
+    } else {
+      var test = require('./src/test');
+      test();
+    }
+  }
+})
